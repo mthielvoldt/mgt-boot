72b6996cc29ac99d3c3450d7c14c1f9d816a42f9
mthielvoldt <mthielvoldt@gmail.com>
2025-04-14 16:16:21 -0400

    Move build-time value from .h to.c file
    Lets this changing data be extracted from the other build targets.
    Which makes determining if code changed much easier.

---
+++ inc/build_time.h
@@ -0,0 +1,8 @@
+#ifndef build_time_h
+#define build_time_h
+
+#include <stdint.h>
+
+uint32_t getBuildTime(void);
+
+#endif
--- tools/genBuildTime.sh
+++ tools/genBuildTime.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 OUT_DIR="$1"
 BUILD_TIME=$(date +%s)
-echo "generating build_time.(h/txt) in $OUT_DIR"
+echo "generating build_time.(c/txt) in $OUT_DIR"
 
-echo "#pragma once" > $OUT_DIR/build_time.h
-echo "#define BUILD_TIME $BUILD_TIME" >> $OUT_DIR/build_time.h
+cat > $OUT_DIR/build_time.c<< EOF
+#include <build_time.h>
+
+uint32_t getBuildTime(void)
+{
+  return ${BUILD_TIME}U;
+}
+EOF
+
 echo $BUILD_TIME > $OUT_DIR/build_time.txt
